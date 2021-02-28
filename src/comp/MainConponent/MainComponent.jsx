c2972c610438182bc9b68a19101d1d22e495bedb
Den-bayia <denis.batia004@gmail.com>
2021-02-28 10:03:50 +0300

    qwe

--- src/comp/MainConponent/MainComponent.jsx
+++ src/comp/MainConponent/MainComponent.jsx
@@ -2,12 +2,15 @@ import { Route } from 'react-router-dom';
 import FirstComponent from '../FirstComponent/FirstComponent';
 import Login from '../Login/Login';
 import s from './MainComponent.module.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const MainComponent = (props) => {
     return(
         <div className={s.grid}>
+            <a className={s.ikon}></a>
             <Route exact component={FirstComponent} path="/" />
             <Route component={Login} path="/login" />
+
         </div>
     )
 }
