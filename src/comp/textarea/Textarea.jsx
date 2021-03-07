9b076d5f92c5018d7a526370c707d0c1e1a79e1f
Den-bayia <denis.batia004@gmail.com>
2021-03-07 21:25:20 +0300

    1.8

--- src/comp/textarea/Textarea.jsx
+++ src/comp/textarea/Textarea.jsx
@@ -1,15 +1,18 @@
 import s from './Textarea.module.css'
 import { LazyLoadImage } from 'react-lazy-load-image-component';
-
+import { NavLink } from "react-router-dom"
 let Textarea = (props) => {
     return (
 
         <div className={s.a} onClick={() => props.dellElement()}>
             <div className={s.text}>{props.data.news_text}</div>
-            <LazyLoadImage className={s.img}  src={props.data.news_img_link}
-                        alt={'ddddd'}>
-                                
-            </LazyLoadImage>
+            <NavLink to={props.data.news_img_link}>
+                 <LazyLoadImage className={s.img}
+                            src={props.data.news_img_link}
+                            alt={'ddddd'}>
+                </LazyLoadImage>
+            </NavLink>
+           
         </div>
     )
 }
